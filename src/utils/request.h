dad48bf30bf4e59fd33fde21459a9087c1299410
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-13 00:36:00 -0300

    Add funcions to request header file

--- src/utils/request.h
+++ src/utils/request.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#include "buffer.h"
+
 enum pop3_req_cmd{
     stat = 0,
     list,
@@ -110,5 +112,36 @@ struct request_parser {
     /** cuantos bytes ya leimos */
     uint8_t i;
 };
+
+/** inicializa el parser */
+void
+request_parser_init (struct request_parser *p);
+
+/** entrega un byte al parser. retorna true si se llego al final  */
+enum request_state
+request_parser_feed (struct request_parser *p, const uint8_t c);
+
+/**
+ * por cada elemento del buffer llama a `request_parser_feed' hasta que
+ * el parseo se encuentra completo o se requieren mas bytes.
+ *
+ * @param errored parametro de salida. si es diferente de NULL se deja dicho
+ *   si el parsing se debió a una condición de error
+ */
+enum request_state
+request_consume(buffer *b, struct request_parser *p, bool *errored);
+
+/**
+ * Permite distinguir a quien usa socks_hello_parser_feed si debe seguir
+ * enviando caracters o no.
+ *
+ * En caso de haber terminado permite tambien saber si se debe a un error
+ */
+bool
+request_is_done(const enum request_state st, bool *errored);
+
+void
+request_close(struct request_parser *p);
+
 
 #endif request_H
