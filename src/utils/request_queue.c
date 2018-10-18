e83219b1e4a421aec81047ad40c7e086b63a601c
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-18 01:17:06 -0300

    Add initial version of request queue

--- src/utils/request_queue.c
+++ src/utils/request_queue.c
@@ -2,4 +2,40 @@
 // Created by francisco on 18/10/18.
 //
 
+#include <stdlib.h>
 #include "request_queue.h"
+
+struct Node {
+    struct request request;
+    struct Node *next;
+}Node;
+
+struct Queue {
+    struct Node *first;
+    struct Node *last;
+    unsigned int size;
+}Queue;
+
+void
+init(struct Queue *q) {
+    q->first = NULL;
+    q->last = NULL;
+    q->size = 0;
+};
+
+void
+queue(struct Queue *q, struct request request) {
+    struct Node *node = malloc(sizeof(Node));
+    node->request = request;
+    q->last->next = node;
+    q->last = node;
+}
+
+struct request
+pop(struct Queue *q) {
+    struct Node *node = q->first;
+    struct request request = node->request;
+    q->first = node->next;
+    free(node);
+    return request;
+}
--- src/utils/request_queue.h
+++ src/utils/request_queue.h
@@ -5,4 +5,7 @@
 #ifndef PROJECT_REQUEST_QUEUE_H
 #define PROJECT_REQUEST_QUEUE_H
 
+#include "request.h"
+
+
 #endif //PROJECT_REQUEST_QUEUE_H
