99221f0099df91daed309a700d34d7b15aeaac79
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-18 01:15:10 -0300

    Add initial version of request queue

---
+++ src/utils/request_queue.c
@@ -0,0 +1,5 @@
+//
+// Created by francisco on 18/10/18.
+//
+
+#include "request_queue.h"
---
+++ src/utils/request_queue.h
@@ -0,0 +1,8 @@
+//
+// Created by francisco on 18/10/18.
+//
+
+#ifndef PROJECT_REQUEST_QUEUE_H
+#define PROJECT_REQUEST_QUEUE_H
+
+#endif //PROJECT_REQUEST_QUEUE_H
