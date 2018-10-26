a16f95ed95cd595a0be13fdf8f297eb403b96210
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-26 17:03:13 -0300

    Add initial version of spcp server credentials validation

--- src/proxy/spcpServerCredentials.c
+++ src/proxy/spcpServerCredentials.c
@@ -2,4 +2,23 @@
 // Created by francisco on 26/10/18.
 //
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 #include "spcpServerCredentials.h"
+
+#define N(x) (sizeof(x)/sizeof((x)[0]))
+
+extern bool
+validate_user(char *username, char *password){
+
+    for(int i = 0; i <  N(USERS); i++) {
+
+        if(     0 == strcmp(username, USERS[i].username) &&
+                0 == strcmp(password, USERS[i].password)
+                ) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/proxy/spcpServerCredentials.h
+++ src/proxy/spcpServerCredentials.h
@@ -5,4 +5,21 @@
 #ifndef PROJECT_SPCPSERVERCREDENTIALS_H
 #define PROJECT_SPCPSERVERCREDENTIALS_H
 
-#endif //PROJECT_SPCPSERVERCREDENTIALS_H
+#include <stdbool.h>
+
+struct user_info {
+    char *username;
+    char *password;
+};
+
+static const struct user_info USERS[] = {
+        {
+            .username = "admin",
+            .password = "admin",
+        }
+};
+
+extern bool
+validate_user(char *username, char *password);
+
+#endif
