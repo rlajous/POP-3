17c0a49b1616a32b588fef52504e3e99637567cf
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-26 02:27:58 -0300

    Add initial version of spcp request

--- src/spcpParsers/spcpRequest.c
+++ src/spcpParsers/spcpRequest.c
@@ -2,4 +2,142 @@
 // Created by francisco on 25/10/18.
 //
 
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "spcpRequest.h"
+#include "../utils/buffer.h"
+
+#define N(x) (sizeof(x)/sizeof((x)[0]))
+
+static void
+remaining_args_set(struct spcp_request_parser *p, const uint8_t n) {
+    p->nargs_i = 0;
+    p->nargs = n;
+}
+
+static int
+remaining_args_is_done(struct spcp_request_parser *p) {
+    return p->nargs_i >= p->nargs;
+}
+
+
+static void
+remaining_arg_set(struct spcp_request_parser *p, const uint8_t n) {
+    p->narg_i = 0;
+    p->narg = n;
+}
+
+static int
+remaining_arg_is_done(struct spcp_request_parser *p) {
+    return p->narg_i >= p->narg;
+}
+
+extern enum spcp_request_state
+request_validate(struct spcp_request_parser *p) {
+    //TODO: validate the command based on the inputs
+}
+
+extern void
+spcp_request_parser_init(struct spcp_request_parser *p){
+    p->state = request_cmd;
+    memset(p->request, 0, sizeof(*(p->request)));
+}
+
+
+extern enum spcp_request_state
+parse_request_cmd(struct spcp_request_parser *p, const uint8_t c) {
+    if(c <= 0x09){
+        p->request->cmd = c;
+        return request_nargs;
+    }
+    return request_error_invalid_command;
+}
+
+extern enum spcp_request_state
+parse_request_nargs(struct spcp_request_parser *p, const uint8_t c) {
+    remaining_args_set(p, c);
+    return request_narg;
+}
+
+extern enum spcp_request_state
+parse_request_narg(struct spcp_request_parser *p, const uint8_t c) {
+    remaining_arg_set(p, c);
+    return request_narg;
+}
+
+extern enum spcp_request_state
+parse_request_arg(struct spcp_request_parser *p, const uint8_t c) {
+
+    //parse arg REMEMBER TO INCREMENT INDEXES
+
+    if(remaining_arg_is_done(p)) {
+        if(remaining_args_is_done(p)){
+            return request_validate(p);
+        } else {
+            return request_narg;
+        }
+    }
+}
+
+extern enum spcp_request_state
+spcp_request_parser_feed(struct spcp_request_parser *p, const uint8_t c){
+    enum spcp_request_state next;
+
+    switch(p->state){
+        case request_cmd:
+            next = parse_request_cmd(p, c);
+            break;
+        case request_nargs:
+            next = parse_request_nargs(p, c);
+            break;
+        case request_narg:
+            next = parse_request_narg(p, c);
+            break;
+        case request_arg:
+            next = parse_request_arg(p, c);
+            break;
+        case request_done:
+        case request_error_invalid_command:
+        case request_error_invalid_arguments:
+        case request_error:
+            next = p->state;
+            break;
+        default:
+            next = request_error;
+            break;
+    }
+    return next;
+}
+
+extern bool
+spcp_request_is_done(const enum spcp_request_state st, bool *errored) {
+    if(st >= request_error && errored != 0) {
+        *errored = true;
+    }
+    return st >= request_done;
+}
+
+extern enum spcp_request_state
+spcp_request_consume(buffer *b, struct spcp_request_parser *p, bool *errored) {
+    enum spcp_request_state st = p->state;
+
+    while(buffer_can_read(b)) {
+        const uint8_t c = buffer_read(b);
+        st = spcp_request_parser_feed(p, c);
+        if(spcp_request_is_done(st, errored)) {
+            break;
+        }
+    }
+    return st;
+}
+
+extern int
+spcp_request_marshall(buffer *b, char *data){
+    return -1;
+}
+
+enum spcp_response_status
+spcp_cmd_resolve(struct spcp_request *request) {
+
+}
--- src/spcpParsers/spcpRequest.h
+++ src/spcpParsers/spcpRequest.h
@@ -1,6 +1,51 @@
 #ifndef PROJECT_REQUEST_H
 #define PROJECT_REQUEST_H
 
+#include <glob.h>
+#include <stdint.h>
+
+/**  The SPCP request is formed as follows:
+ *
+ *      +-----+-------+----------+----------+...+----------+----------+
+ *      | CMD | NARGS | ARGLEN 1 |   ARG 1  |   | ARGLEN N |   ARG N  |
+ *      +-----+-------+----------+----------+...+----------+----------+
+ *      |  1  |   1   |    1    | Variable |   |    1     | Variable |
+ *      +-----+-------+----------+----------+...+----------+----------+
+ *
+ *   Where:
+ *
+ *        o  CMD
+ *           o  USER                        X'00'
+ *           o  PASS                        X'01'
+ *           o  GET CONCURRENT CONNECTIONS  X'02'
+ *           o  GET TRANSFERRED BYTES       X'03'
+ *           o  GET HISTORICAL ACCESSES     X'04'
+ *           o  GET ACTIVE TRANSFORMATION   X'05'
+ *           o  QUIT                        X'06'
+ *           o  SET BUFFER SIZE             X'07'
+ *           o  CHANGE TRANSFORMATION       X'08'
+ *           o  SET TIMEOUTS                X'09'
+ *
+ *
+ *        o  NARGS      number of arguments sent
+ *        o  ARGLEN N   length of the nth argument
+ *        o  ARG N      the nth argument
+ *
+ */
+
+enum spcp_request_state {
+    request_cmd,
+    request_nargs,
+    request_narg,
+    request_arg,
+
+    request_done,
+
+    request_error_invalid_command,
+    request_error_invalid_arguments,
+    request_error,
+};
+
 enum spcp_response_status {
     success             = 0x00,
     auth_err            = 0x01,
@@ -9,28 +54,38 @@ enum spcp_response_status {
     err                 = 0x04,
 };
 
-
 enum spcp_request_cmd {
-    spcp_user,
-    spcp_pass,
-    spcp_concurrent_connections,
-    spcp_transfered_bytes,
-    spcp_historical_accesses,
-    spcp_active_transformation,
-    spcp_set_buffer_size,
-    spcp_change_transformation,
-    spcp_set_timeouts,
-    spcp_quit,
+    user,
+    pass,
+    concurrent_connections,
+    transfered_bytes,
+    historical_accesses,
+    active_transformation,
+    set_buffer_size,
+    change_transformation,
+    set_timeouts,
+    quit,
 };
 
 struct spcp_request {
     enum spcp_request_cmd cmd;
-    char *arg1;
-    char *arg2
+    uint8_t *arg1;
+    uint8_t *arg2;
 };
 
 struct spcp_request_parser {
-    struct spcp_request request;
+    struct spcp_request         *request;
+    enum   spcp_request_state   state;
+    /** cantidad de argumentos */
+    uint8_t nargs;
+    /** argumentos ya leidos */
+    uint8_t nargs_i;
+    /** ultimo narg parseado */
+    uint8_t narg;
+    /** bytes del argumento ya leidos*/
+    uint8_t narg_i;
+
+
 };
 
 #endif
